f275de54cbede5916ff68046e8037ab141f15eb3
mcantop <maciejcantop@icloud.com>
2022-09-12 21:26:15 +0200

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Maciej on 12/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
